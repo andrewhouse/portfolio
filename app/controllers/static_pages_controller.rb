a64068535b45e2b58944ec2bfeb97b677bd6c911
Andrew House <andrewhouse621@gmail.com>
2014-10-22 14:41:17 -0400

    Add Experience Section

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
   end
+  def download
+    file_path = "#{Rails.root}/public/resume.pdf"
+    send_file file_path, :filename => 'resume.pdf', :disposition => 'attachment'
+  end
 end
